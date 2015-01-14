9d6c9ecf9d3d80cdd5bb93f70938d2eb5a28ed6c
Alex Pate <proto.merkabah@gmail.com>
2015-01-14 14:31:55 -0500

    variable operators allowed

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
 var num_one      = document.getElementById('num_one'),
     num_two      = document.getElementById('num_two'),
     calculateBtn = document.getElementById('calculateBtn'),
-    answer       = document.getElementById('answer');
+    answer       = document.getElementById('answer'),
+    operator     = operator || '+';
 
 
 
-function createButton(name, operator) {
+function createButton(op) {
   var elem = document.createElement('input');
   elem.type = 'submit';
-  elem.calculate = operator;
+  elem.value = op;
+  elem.onclick = function() {
+    operator = op;
+  };
   return elem;
 };
 
-var add_btn = createButton('Add', '+');
-var sub_btn = createButton('Substract', '-');
-var mul_btn = createButton('Multiply', '*');
-var sub_btn = createButton('Divide', '/');
+var add_btn = createButton('+');
+var sub_btn = createButton('-');
+var mul_btn = createButton('*');
+var div_btn = createButton('/');
 
+calculateBtn.parentNode.insertBefore(div_btn, calculateBtn);
+calculateBtn.parentNode.insertBefore(mul_btn, calculateBtn);
+calculateBtn.parentNode.insertBefore(sub_btn, calculateBtn);
+calculateBtn.parentNode.insertBefore(add_btn, calculateBtn);
 
-
-
+var operation = {
+  '+' : function(a, b) { return a + b; },
+  '-' : function(a, b) { return a - b; },
+  '*' : function(a, b) { return a * b; },
+  '/' : function(a, b) { return a / b; }
+};
 
 calculateBtn.onclick = function() {
   var result; 
   op1 = parseFloat(num_one.value);
   op2 = parseFloat(num_two.value);
 
-  result = op1 + op2;
+  result = operation[operator](op1, op2);
   answer.innerHTML = result;
 }
