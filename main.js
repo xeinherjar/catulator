835078ebe9ca63a4413f18124b7f4d91fb1db788
Alex Pate <proto.merkabah@gmail.com>
2015-01-14 14:49:49 -0500

    Added a function for each operation...

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
-var num_one      = document.getElementById('num_one'),
-    num_two      = document.getElementById('num_two'),
-    calculateBtn = document.getElementById('calculateBtn'),
-    answer       = document.getElementById('answer'),
-    operator     = operator || '+';
+var num_one = document.getElementById('num_one'),
+    num_two = document.getElementById('num_two'),
+    add_Btn = document.getElementById('add_Btn'),
+    sub_Btn = document.getElementById('sub_Btn'),
+    mul_Btn = document.getElementById('mul_Btn'),
+    div_Btn = document.getElementById('div_Btn'),
+    answer  = document.getElementById('answer');
 
 
 
-function createButton(op) {
-  var elem = document.createElement('input');
-  elem.type = 'submit';
-  elem.value = op;
-  elem.onclick = function() {
-    operator = op;
-  };
-  return elem;
-};
-
-var add_btn = createButton('+');
-var sub_btn = createButton('-');
-var mul_btn = createButton('*');
-var div_btn = createButton('/');
-
-calculateBtn.parentNode.insertBefore(div_btn, calculateBtn);
-calculateBtn.parentNode.insertBefore(mul_btn, calculateBtn);
-calculateBtn.parentNode.insertBefore(sub_btn, calculateBtn);
-calculateBtn.parentNode.insertBefore(add_btn, calculateBtn);
-
-var operation = {
-  '+' : function(a, b) { return a + b; },
-  '-' : function(a, b) { return a - b; },
-  '*' : function(a, b) { return a * b; },
-  '/' : function(a, b) { return a / b; }
-};
-
-calculateBtn.onclick = function() {
+add_Btn.onclick = function() {
   var result; 
   op1 = parseFloat(num_one.value);
   op2 = parseFloat(num_two.value);
 
-  result = operation[operator](op1, op2);
+  result = op1 + op2; 
+  answer.innerHTML = result;
+}
+
+sub_Btn.onclick = function() {
+  var result; 
+  op1 = parseFloat(num_one.value);
+  op2 = parseFloat(num_two.value);
+
+  result = op1 - op2; 
+  answer.innerHTML = result;
+}
+
+mul_Btn.onclick = function() {
+  var result; 
+  op1 = parseFloat(num_one.value);
+  op2 = parseFloat(num_two.value);
+
+  result = op1 * op2; 
+  answer.innerHTML = result;
+}
+
+div_Btn.onclick = function() {
+  var result; 
+  op1 = parseFloat(num_one.value);
+  op2 = parseFloat(num_two.value);
+
+  result = op1 / op2; 
   answer.innerHTML = result;
 }
