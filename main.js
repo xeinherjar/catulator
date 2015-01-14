b4629d4b0adfdfb19682ea6c9ff68e508795daa1
Alex Pate <proto.merkabah@gmail.com>
2015-01-14 17:26:33 -0500

    heading home

--- main.js
+++ main.js
@@ -1,45 +1,35 @@
-var num_one = document.getElementById('num_one'),
-    num_two = document.getElementById('num_two'),
-    add_Btn = document.getElementById('add_Btn'),
-    sub_Btn = document.getElementById('sub_Btn'),
-    mul_Btn = document.getElementById('mul_Btn'),
-    div_Btn = document.getElementById('div_Btn'),
-    answer  = document.getElementById('answer');
+var result = document.getElementById('result');
+var num_btns = document.getElementsByClassName('num');
+var op_btns  = document.getElementsByClassName('op');
+var clear = false,
+    total = 0;
 
+result.innerHTML = 0;
 
+var ops = {
+  'add': function(a, b) { return a + b; },
+  'sub': function(a, b) { return a - b; },
+  'mul': function(a, b) { return a * b; },
+  'div': function(a, b) { return a / b; }
+};
 
-add_Btn.onclick = function() {
-  var result; 
-  op1 = parseFloat(num_one.value);
-  op2 = parseFloat(num_two.value);
-
-  result = op1 + op2; 
-  answer.innerHTML = result;
+for (var i = 0; i < num_btns.length; i++) {
+  var value = num_btns[i].id; 
+  num_btns[i].onclick = function(value) { 
+    if (result.innerHTML === "0" || clear) {
+      result.innerHTML = "";
+    }
+    result.innerHTML += value.target.id; 
+    clear = false;
+  };
 }
 
-sub_Btn.onclick = function() {
-  var result; 
-  op1 = parseFloat(num_one.value);
-  op2 = parseFloat(num_two.value);
-
-  result = op1 - op2; 
-  answer.innerHTML = result;
-}
-
-mul_Btn.onclick = function() {
-  var result; 
-  op1 = parseFloat(num_one.value);
-  op2 = parseFloat(num_two.value);
-
-  result = op1 * op2; 
-  answer.innerHTML = result;
-}
-
-div_Btn.onclick = function() {
-  var result; 
-  op1 = parseFloat(num_one.value);
-  op2 = parseFloat(num_two.value);
-
-  result = op1 / op2; 
-  answer.innerHTML = result;
+for (var i = 0; i < op_btns.length; i++) {
+  var op = op_btns[i].id;
+  op_btns[i].onclick = function(op) {
+    op = op.target.id;
+    total = ops[op](total, parseFloat(result.innerHTML));
+    result.innerHTML = total;
+    clear = true;
+  };
 }
