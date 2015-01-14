48a82058c8b5519527c48c2928603fedc30dea55
Alex Pate <proto.merkabah@gmail.com>
2015-01-14 12:42:57 -0500

    Variables

---
+++ main.js
@@ -0,0 +1,6 @@
+var num_one      = document.getElementById('num_one'),
+    num_two      = document.getElementById('num_two'),
+    calculateBtn = document.getElementById('calculateBtn'),
+    answer       = document.getElementById('answer');
+
+
