c34f2109320c49e71dd7321b6156891b8a93e95d
Alex Pate <proto.merkabah@gmail.com>
2015-01-14 12:52:05 -0500

    JS for simple version

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ var num_one      = document.getElementById('num_one'),
     calculateBtn = document.getElementById('calculateBtn'),
     answer       = document.getElementById('answer');
 
+calculateBtn.onclick = function() {
+  var result; 
+  op1 = parseInt(num_one.value, 10);
+  op2 = parseInt(num_two.value, 10);
 
+  result = op1 + op2;
+  answer.innerHTML = result;
+}
